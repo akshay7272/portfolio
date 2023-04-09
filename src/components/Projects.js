a2d56e0c5ec5cdc445e27f3008da8e85940b9abd
akshay7272 <kashyap72akshay@gmail.com>
2023-04-09 12:02:32 +0530

    added my react js personal learning  projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -6,12 +6,18 @@ import cellwork from "../images/cellwork.jpg";
 import urbz from "../images/urbz.jpg";
 import lambandbeef from "../images/lambandbeef.jpg";
 import account from "../images/account.jpg";
+import todo from "../images/todo.jpg";
+import weather from "../images/weather.jpg";
+import portfolio from "../images/portfolio.jpg";
 function Projects() {
   return (
     <>
-      <section>
+      <section className="pb-9">
         <div className="mt-0">
           <div className=" mx-auto max-w-7xl px-4 lg:px-8">
+            <h1 className="text-4xl text-center text-white">
+              <i>Contribution At Job</i>
+            </h1>
             <div className="mx-auto mt-1 grid max-w-2xl grid-cols-1 gap-y-16 gap-x-8 pt-10 lg:mt-10 md:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3 md:grid-cols-2">
               <div className="mx-auto max-w-sm bg-neutral-100 border border-gray-200 rounded-lg shadow ">
                 <img className="rounded-t-lg" src={lambandbeef} alt="" />
@@ -224,7 +230,6 @@ function Projects() {
                   </a>
                 </div>
               </div>
-
               <div className="mx-auto max-w-sm bg-neutral-50 border border-gray-200 rounded-lg shadow ">
                 <img className="rounded-t-lg" src={urbz} alt="" />
                 <div className="p-5">
@@ -252,6 +257,133 @@ function Projects() {
                     <svg
                       aria-hidden="true"
                       className="w-4 h-4 ml-2 -mr-1 "
+                      fill="currentColor"
+                      viewBox="0 0 20 20"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                        clipRule="evenodd"
+                      ></path>
+                    </svg>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className=" mx-auto max-w-7xl px-4 lg:px-8">
+            <h1 className="text-4xl text-center pt-7 mt-7 text-white">
+              <i>Personal React Projects</i>
+            </h1>
+            <div className="mx-auto mt-1 grid max-w-2xl grid-cols-1 gap-y-16 gap-x-8 pt-10 lg:mt-10 md:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3 md:grid-cols-2">
+              <div className="mx-auto max-w-sm bg-neutral-50 border border-gray-200 rounded-lg shadow ">
+                <img
+                  className="rounded-t-lg"
+                  src={portfolio}
+                  alt="accountability"
+                />
+                <div className="p-5">
+                  <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 ">
+                    Portfolio Website
+                  </h5>
+                  <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                    This is my personal portfolio website where you can each
+                    detail about me as well as of my all projects. This project
+                    built using react js and tailwind css.
+                  </p>
+                  <p className="pt-2">
+                    <b>Front end:</b> React js, JSX & Tailwind CSS.
+                  </p>
+
+                  <a
+                    href="https://ak-port-folio.netlify.app/"
+                    target="_blank"
+                    rel="noreferrer"
+                    className="inline-flex items-center px-3 py-2 text-sm font-medium text-center mt-2 text-white bg-zinc-600 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                  >
+                    Visit Website
+                    <svg
+                      aria-hidden="true"
+                      className="w-4 h-4 ml-2 -mr-1"
+                      fill="currentColor"
+                      viewBox="0 0 20 20"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                        clipRule="evenodd"
+                      ></path>
+                    </svg>
+                  </a>
+                </div>
+              </div>
+              <div className="mx-auto max-w-sm bg-neutral-100 border border-gray-200 rounded-lg shadow ">
+                <img className="rounded-t-lg" src={todo} alt="" />
+                <div className="p-5">
+                  <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 ">
+                    Todo Application
+                  </h5>
+
+                  <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                    This is a React js project with ant design to set your todo
+                    and perform different tasks like updating, deleting,
+                    sorting, filtering and searching etc.
+                  </p>
+                  <p className="pt-2">
+                    <b>Front end:</b> React js, JSX & AntDesign.
+                  </p>
+
+                  <a
+                    href="https://ak-react-toto.netlify.app/"
+                    target="_blank"
+                    rel="noreferrer"
+                    className="mt-2 inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-zinc-600 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                  >
+                    Visit Website
+                    <svg
+                      aria-hidden="true"
+                      className="w-4 h-4 ml-2 -mr-1"
+                      fill="currentColor"
+                      viewBox="0 0 20 20"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                        clipRule="evenodd"
+                      ></path>
+                    </svg>
+                  </a>
+                </div>
+              </div>
+              <div className="mx-auto max-w-sm bg-neutral-50 border border-gray-200 rounded-lg shadow ">
+                <img className="rounded-t-lg" src={weather} alt="" />
+                <div className="p-5">
+                  <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 ">
+                    Weather Application
+                  </h5>
+                  <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                    This is a ReactJS Weather Application,Which shows the
+                    current temrature of different locations all over the
+                    world.Used Weather API to get details from different
+                    locations.
+                  </p>
+                  <p className="pt-2">
+                    <b>Front end:</b>React JS, JSX & API Fetch.
+                  </p>
+
+                  <a
+                    href="https://react-weather-app-ak.netlify.app"
+                    target="_blank"
+                    rel="noreferrer"
+                    className="mt-2 inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-zinc-600 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                  >
+                    Visit Website
+                    <svg
+                      aria-hidden="true"
+                      className="w-4 h-4 ml-2 -mr-1"
                       fill="currentColor"
                       viewBox="0 0 20 20"
                       xmlns="http://www.w3.org/2000/svg"
