1d97119b48eff4fe5d12937df75d936630660cce
akshay.k <akshay.k@gai.co.in>
2023-05-18 16:44:01 +0530

    added gratisbooks working link

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -299,7 +299,7 @@ function Projects() {
                   </p>
 
                   <a
-                    href="https://gratisbooks.gailabs.in/"
+                    href="https://gratisbooks.netlify.app/"
                     target="_blank"
                     rel="noreferrer"
                     className="mt-2 inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-zinc-600 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
