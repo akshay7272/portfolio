121e5eb7346aecc350ad62693cbe5ebf4f7782cc
akshay.k <akshay.k@gai.co.in>
2023-05-18 15:02:18 +0530

    new project added

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -62,48 +62,7 @@ function Projects() {
                   </a>
                 </div>
               </div>
-              <div className="mx-auto max-w-sm bg-neutral-50 border border-gray-200 rounded-lg shadow ">
-                <img className="rounded-t-lg" src={gratis} alt="" />
-                <div className="p-5">
-                  <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 ">
-                    Gratis Books
-                  </h5>
-                  <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-                    This is a ReactJS PWA Project. Which provides you to simply
-                    donate book to another user without charge or recompense.
-                  </p>
-                  <p className="pt-2">
-                    <b>My Contribution</b>
-                    <span className="p-1">(Built whole project)</span>
-                  </p>
-                  <p className="pt-2">
-                    <b>Front end:</b> Html, CSS, React, MUI. <b>Back end:</b>
-                    Firebase.
-                  </p>
 
-                  <a
-                    href="https://gratisbooks.gailabs.in/"
-                    target="_blank"
-                    rel="noreferrer"
-                    className="mt-2 inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-zinc-600 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                  >
-                    Visit Website
-                    <svg
-                      aria-hidden="true"
-                      className="w-4 h-4 ml-2 -mr-1"
-                      fill="currentColor"
-                      viewBox="0 0 20 20"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path
-                        fillRule="evenodd"
-                        d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-                        clipRule="evenodd"
-                      ></path>
-                    </svg>
-                  </a>
-                </div>
-              </div>
               <div className="mx-auto max-w-sm bg-neutral-50 border border-gray-200 rounded-lg shadow ">
                 <img className="rounded-t-lg" src={cellwork} alt="" />
                 <div className="p-5">
@@ -302,6 +261,48 @@ function Projects() {
                     target="_blank"
                     rel="noreferrer"
                     className="inline-flex items-center px-3 py-2 text-sm font-medium text-center mt-2 text-white bg-zinc-600 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                  >
+                    Visit Website
+                    <svg
+                      aria-hidden="true"
+                      className="w-4 h-4 ml-2 -mr-1"
+                      fill="currentColor"
+                      viewBox="0 0 20 20"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                        clipRule="evenodd"
+                      ></path>
+                    </svg>
+                  </a>
+                </div>
+              </div>
+              <div className="mx-auto max-w-sm bg-neutral-50 border border-gray-200 rounded-lg shadow ">
+                <img className="rounded-t-lg" src={gratis} alt="" />
+                <div className="p-5">
+                  <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 ">
+                    Gratis Books
+                  </h5>
+                  <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                    This is a ReactJS PWA Project. Which provides you to simply
+                    donate book to another user without charge or recompense.
+                  </p>
+                  <p className="pt-2">
+                    <b>My Contribution</b>
+                    <span className="p-1">(Built whole project)</span>
+                  </p>
+                  <p className="pt-2">
+                    <b>Front end:</b> Html, CSS, React, MUI. <b>Back end:</b>
+                    Firebase.
+                  </p>
+
+                  <a
+                    href="https://gratisbooks.gailabs.in/"
+                    target="_blank"
+                    rel="noreferrer"
+                    className="mt-2 inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-zinc-600 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                   >
                     Visit Website
                     <svg
